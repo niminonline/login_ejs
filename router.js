493a28fa5309ce5fde6565c7e5306895946ec211
niminonline <niminonline@gmail.com>
2023-02-23 18:49:37 +0530

    webdone2

--- router.js
+++ router.js
@@ -10,10 +10,15 @@ const credentials={
 
 router.get('/',(req,res)=>
 {
-    res.render("login");
+    
+    res.render("login",{x:""});
 }) 
 
-const ejsData=[{image:"",title:"Express",description:"",btnText:""}];
+const ejsData=[{image:"/assets/images/javascript.png",title:"JavaScript",description:"JavaScript is a scripting language that enables you to create dynamically updating content, control multimedia, animate images, and pretty much everything else.",btnText:"Js"},
+                {image:"/assets/images/express.png",title:"Express",description:"Express web framework (Node.js/JavaScript) Express is a popular unopinionated web framework, written in JavaScript and hosted within the Node.js runtime environment. ",btnText:"expressJs"},
+                {image:"/assets/images/node.png",title:"NodeJs",description:"Node.js is a cross-platform, open-source server environment that can run on Windows, Linux, Unix, macOS, and more. Node.js is a back-end JavaScript runtime environment.",btnText:"nJs"},
+                {image:"/assets/images/ejs.png",title:"EJS",description:"EJS is a tool for generating web pages that can include dynamic data and can share templated pieces with other web pages (such as common headers/footers). It is not a front-end framework.",btnText:"eJs"}
+];
 router.get('/home',(req,res)=>
 {
     res.render("home",{data:ejsData});
@@ -25,7 +30,12 @@ router.post("/login",(req,res)=>
     if ((req.body.username== credentials.username)&& (req.body.password== credentials.password))
     {
         // res.end("Successfull")
+       
+       res.render("login",{x:"Login successfully"});
+       setTimeout(() => {
         res.redirect("home");
+        
+       }, 3000); 
     }
     else
     res.end("Invalid")
--- server.js
+++ server.js
@@ -14,6 +14,20 @@ app.use('/public',express.static(path.join(__dirname+"public_html")));
 app.use(router);
 app.use("/assets",express.static(path.join(__dirname)+ 'public_html/assets'));
 
+// function emailValidation(){
+// {
+// var mailregex = /\b^[^ ][a-z.\-_0-9]+@[a-z0-9]+\.[a-z]{2,3}\b/;
+// if(!(document.getElementById("email").value).match(mailregex))
+// {
+//   document.getElementById("email-error").innerText= ("Enter a valid email id");
+//   emailvalid =false;
+// }
+// else
+// {
+//   document.getElementById("email-error").innerText= "";
+
+// emailvalid=true;
+// } } }
 
 
 
