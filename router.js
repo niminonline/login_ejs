44b01e3e6361a2dce01827d41d0f20f4f625a0d7
niminonline <niminonline@gmail.com>
2023-02-23 16:24:04 +0530

    webdone

--- router.js
+++ router.js
@@ -1,16 +1,31 @@
 import express from 'express';
-import router from express.Router();
-import bodyParser from 'body-parser';
+const router= express.Router();
+
 
 const credentials={
     username:"admin@email.com",
     password:"admin@123"
 }
+
+
+router.get('/',(req,res)=>
+{
+    res.render("login");
+}) 
+
+const ejsData=[{image:"",title:"Express",description:"",btnText:""}];
+router.get('/home',(req,res)=>
+{
+    res.render("home",{data:ejsData});
+})
+
+
 router.post("/login",(req,res)=>
 {
     if ((req.body.username== credentials.username)&& (req.body.password== credentials.password))
     {
-        res.redirect("/home")
+        // res.end("Successfull")
+        res.redirect("home");
     }
     else
     res.end("Invalid")
@@ -20,4 +35,4 @@ router.post("/login",(req,res)=>
 
 
 
-export router;
+export default router;
--- server.js
+++ server.js
@@ -2,19 +2,19 @@ import express from "express";
 import path, { dirname } from 'path';
 import url from 'url'
 import bodyParser from "body-parser";
-import router from "router";
+
+import router from "./router.js";
 
 const app= express();
 const port= process.env.PORT|3000;
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 app.set('view engine','ejs');
-console.log();
-app.use(express.static(path.join(__dirname+"public_html")));
+app.use(bodyParser.urlencoded({extended:true}))
+app.use('/public',express.static(path.join(__dirname+"public_html")));
+app.use(router);
+app.use("/assets",express.static(path.join(__dirname)+ 'public_html/assets'));
 
-app.get('/',(req,res)=>
-{
-    res.render("login");
-})
+
 
 
 
